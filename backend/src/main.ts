4461033e4e319f3c0f399f2ea183329a4df88ebf
NathanielMention <nathanielmention@gmail.com>
2023-04-05 00:09:26 -0400

    (backend): add auth routes to main.ts file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,7 @@ import logger from "./utils/logger";
 import { CORS_ORIGIN } from "./constants";
 import helmet from "helmet";
 import userRoute from "./modules/user/user.route";
+import authRoute from "./modules/auth/auth.route";
 import deserializeUser from "./middleware/deserializeUser";
 
 const PORT = process.env.PORT || 4000;
@@ -24,6 +25,7 @@ app.use(helmet());
 app.use(deserializeUser);
 
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 const server = app.listen(PORT, async () => {
   await connectToDatabase();
