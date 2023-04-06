5629df84f02f25238f92f92b1c6bdc5e5873845e
NathanielMention <nathanielmention@gmail.com>
2023-04-06 01:13:37 -0400

    (client): add logo, remove files, edit _doc file

--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { createGetInitialProps } from "@mantine/next";
 
+//custom doc can update tags used to render page. this file only renders on server. no event handlers work on this file
 export default function Document() {
   return (
     <Html lang="en">
@@ -11,3 +13,5 @@ export default function Document() {
     </Html>
   )
 }
+
+Document.getInitialProps = createGetInitialProps();
